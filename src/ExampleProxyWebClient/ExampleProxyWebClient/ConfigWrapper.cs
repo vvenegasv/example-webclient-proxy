6945bb524d14fa043dd4eb2d05ea91b2b6cb9445
Vladimir Venegas <vvenegas@infodinamica.cl>
2017-05-29 11:47:37 -0400

    Se sube la primera versión

---
+++ src/ExampleProxyWebClient/ExampleProxyWebClient/ConfigWrapper.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleProxyWebClient
+{
+    internal class ConfigWrapper
+    {
+        private static volatile ConfigWrapper _instance;
+        private static object _syncRoot = new Object();
+
+        private ConfigWrapper() { }
+
+        public static ConfigWrapper Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (_syncRoot)
+                    {
+                        if (_instance == null)
+                            _instance = new ConfigWrapper();
+                    }
+                }
+                return _instance;
+            }
+        }
+        
+        public string KeyGoogleMaps
+        {
+            get
+            {
+                var key = ConfigurationManager.AppSettings["googlemap-key"];
+                if (key == null || string.IsNullOrWhiteSpace(key))
+                    return string.Empty;
+                else
+                    return key;
+            }
+        }
+
+        public string ProxyUrl
+        {
+            get
+            {
+                var key = ConfigurationManager.AppSettings["proxy-url"];
+                if (key == null || string.IsNullOrWhiteSpace(key))
+                    return string.Empty;
+                else
+                    return key;
+            }
+        }
+
+    }
+}
---
+++ src/ExampleProxyWebClient/ExampleProxyWebClient/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ExampleProxyWebClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var encodedAddress = "Andrés Bello 2040, Santiago, Región Metropolitana";
+                var key = ConfigWrapper.Instance.KeyGoogleMaps;
+                using(var client = new WebClient())
+                {
+                    if (!string.IsNullOrWhiteSpace(ConfigWrapper.Instance.ProxyUrl))
+                    {
+                        var proxy = new WebProxy(ConfigWrapper.Instance.ProxyUrl, true);
+                        client.Proxy = proxy;
+                        Console.WriteLine($"Proxy configurated at '{ConfigWrapper.Instance.ProxyUrl}'");
+                    }
+                    var data = client.DownloadData($"https://maps.googleapis.com/maps/api/geocode/xml?sensor=false&key={key}&address={encodedAddress}");
+
+                    using (Stream stream = new MemoryStream(data))
+                    {
+                        XDocument document = XDocument.Load(new StreamReader(stream));
+                        XElement longitudeElement = document.Descendants("lng").FirstOrDefault();
+                        XElement latitudeElement = document.Descendants("lat").FirstOrDefault();
+                        double lat = 0;
+                        double lon = 0;
+
+                        if (latitudeElement != null && longitudeElement != null)
+                            if (double.TryParse(latitudeElement.Value, out lat))
+                                if (double.TryParse(longitudeElement.Value, out lon))
+                                    Console.WriteLine($"latitude:{lat}, longitude:{lon}");
+                    }
+                }
+            
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+            Console.WriteLine("Proceso finalizado. Presione cualquier tecla para cerrar.");
+            Console.ReadKey();
+        }
+    }
+}
